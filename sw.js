d6d6ca7b182152d9b0e8222719176d43a61d1bf5
branzfamily01 <branz.family.01@gmail.com>
2026-08-16 00:12:16 +0900

    Refresh PWA cache for sound and reward update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='race-to-study-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/supercar.webp'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='race-to-study-v5';
+const ASSETS=['./','./index.html','./styles.css','./app-core.js','./app-views.js','./app-runtime.js','./app-patch.js','./manifest.webmanifest','./icon.svg','./assets/supercar.webp'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
